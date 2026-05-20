3fcc19a3327481e20a82e72cc9966d79707ed6df
lanangwisana <lanangwisana@gmail.com>
2026-05-20 16:05:18 +0700

    feat(verifikator-pusat): add tanggalAwalPenelaahan state to buat penelaahan pusat store

--- store/useBuatPenelaahanPusatStore.ts
+++ store/useBuatPenelaahanPusatStore.ts
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 export interface BuatPenelaahanPusatFormState {
   formData: {
     judulPenelaahan: string;
+    tanggalAwalPenelaahan: string;
     tanggalPenelaahan: string;
     jenisUnsur: string[];
     adminVerifikator: string[];
@@ -13,6 +14,7 @@ export interface BuatPenelaahanPusatFormState {
 
 const initialFormState: BuatPenelaahanPusatFormState['formData'] = {
   judulPenelaahan: '',
+  tanggalAwalPenelaahan: '',
   tanggalPenelaahan: '',
   jenisUnsur: [],
   adminVerifikator: [],
